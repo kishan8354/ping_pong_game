a87e174a3e2aee81bcadfb289bf9fbf23b976447
kishan kushavaha <103376075+kishan8354@users.noreply.github.com>
2023-09-05 19:00:29 +0530

    Add files via upload

---
+++ pinp pong game/kish.js
@@ -0,0 +1,14 @@
+
+class Person {
+constructor(name) {
+    this.name = name;
+}
+get name() {
+    return this._name;
+}
+set name(value) {
+    this._name = value;
+}
+}
+var person = new Person("James");
+
